45baa89af3fd1a6dcd9189cd24520f365f26f702
Elutayo <elutayojohnson@gmail.com>
2025-06-26 08:33:58 +0100

    adding task 5

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,9 @@
-INSERT INTO Customer(customer_id, customer_name, email, address) 
-VALUES  (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave")
+-- sql = "INSERT INTO Customer(customer_id, customer_name, email, address)  VALUES (%s, %s, %s, %s)"
+-- val = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave")
+
+
+
+sql = "INSERT INTO Customers (custome_id, customer_name, email, address) VALUES (%s, %s)"
+val = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
+mycursor.execute(sql, val)
+mydb.commit() 
