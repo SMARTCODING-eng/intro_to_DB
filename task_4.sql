244634e19c750bb11e7264bdc12bfdcbfb18ed04
Elutayo <elutayojohnson@gmail.com>
2025-06-26 07:43:10 +0100

    Adding new file

---
+++ task_4.sql
@@ -0,0 +1,15 @@
+
+SELECT
+    TABLE_NAME,
+    COLUMN_NAME,
+    DATA_TYPE,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    EXTRA,
+    COLUMN_DEFAULT 
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE   
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME IN ('Authors', 'Books', 'Customers', 'Orders', 'Order_Details');
